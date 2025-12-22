3ad6938f347c9139f29c1d204937a3923cb7a4aa
drimdrim2002 <drimdrim2002@naver.com>
2025-12-23 00:07:20 +0900

    dto converter test 반영

---
+++ src/test/java/org/acme/solver/util/DtoConverterTest.java
@@ -0,0 +1,42 @@
+package solver.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.acme.api.dto.PlanningRequest;
+import org.acme.solver.model.EmployeeSchedule;
+import org.acme.solver.util.DtoConverter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@QuarkusTest
+public class DtoConverterTest {
+
+    @Inject
+    DtoConverter dtoConverter;
+
+    @Inject
+    ObjectMapper objectMapper;
+
+    @Test
+    public void testToEmployeeSchedule() throws IOException {
+        // Given
+        PlanningRequest request;
+        try (InputStream inputStream = getClass().getResourceAsStream("/json/sample.json")) {
+            if (inputStream == null) {
+                throw new IOException("Resource not found: /json/sample.json");
+            }
+            request = objectMapper.readValue(inputStream, PlanningRequest.class);
+        }
+
+        // When
+        EmployeeSchedule schedule = dtoConverter.toEmployeeSchedule(request);
+
+        // Then
+        assertNotNull(schedule, "The converted schedule should not be null");
+    }
+}
