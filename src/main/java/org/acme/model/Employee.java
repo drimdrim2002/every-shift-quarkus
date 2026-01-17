01ff3da1555f0c9ed0dc8b105647546d2a3a6e04
drimdrim2002 <drimdrim2002@naver.com>
2026-01-17 22:23:24 +0900

    toString 변경

--- src/main/java/org/acme/model/Employee.java
+++ src/main/java/org/acme/model/Employee.java
@@ -77,6 +77,9 @@ public class Employee {
 
     @Override
     public String toString() {
-        return id;
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
     }
 }
